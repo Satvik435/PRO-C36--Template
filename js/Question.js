be4bf437ad777bc3811df0bc568a60bb043c6fa9
Satvik435 <86778274+Satvik435@users.noreply.github.com>
2021-12-09 16:58:01 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -26,8 +26,8 @@ class Question {
     this.title.position(350, 0);
 
     // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-     this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    //this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+   // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
 
     this.question.position(150, 80);
